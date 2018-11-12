77873bd91f18deaec8eb99ab129ddb762951027a
Brynn Shepherd <brynn.shepherd@gmail.com>
2018-11-12 17:24:20 -0500

    seeding some seeds

--- models.js
+++ models.js
@@ -1,5 +1,43 @@
+/* eslint-disable camelcase */
 const Sequelize = require('sequelize');
-const db = new Sequelize('postgres://localhost:5432/plantr');
+const db = new Sequelize('postgres://localhost:5432/plantr', {
+  logging: false,
+});
 
+const Gardener = db.define('gardener', {
+  name: {
+    type: Sequelize.STRING,
+  },
+  age: {
+    type: Sequelize.INTEGER,
+  },
+});
 
-module.exports =db;
+const Plot = db.define('plot', {
+  size: {
+    type: Sequelize.INTEGER,
+  },
+  shaded: {
+    type: Sequelize.BOOLEAN,
+  },
+});
+
+const Vegetable = db.define('vegetable', {
+  name: {
+    type: Sequelize.STRING,
+  },
+  color: {
+    type: Sequelize.STRING,
+  },
+  planted_on: {
+    type: Sequelize.DATE,
+  },
+});
+
+Plot.belongsTo(Gardener);
+Gardener.hasOne(Plot);
+Vegetable.belongsToMany(Plot, { through: 'vegetable_plot' });
+Plot.belongsToMany(Vegetable, { through: 'vegetable_plot' });
+Gardener.belongsTo(Vegetable, { as: 'favorite_vegetable' });
+
+module.exports = { db, Plot, Gardener, Vegetable };
--- seed.js
+++ seed.js
@@ -1,11 +1,46 @@
-const db = require('./models');
+/* eslint-disable camelcase */
+const models = require('./models');
 
+const corn = models.Vegetable.create({
+  name: 'corn',
+  color: 'yellow',
+  planted_on: Date.now(),
+});
 
-db.sync({force:true})
-.then(()=>{
+const kale = models.Vegetable.create({
+  name: 'kale',
+  color: 'green',
+  planted_on: Date.now(),
+});
+
+const mushroom = models.Vegetable.create({
+  name: 'mushroom',
+  color: 'brown',
+  planted_on: Date.now(),
+});
+
+const wenyi = models.Gardener.create({
+  name: 'wenyi',
+  age: 26,
+});
+
+const brynn = models.Gardener.create({
+  name: 'brynn',
+  age: 30,
+});
+
+const plot = models.Plot.create({
+  size: 100,
+  shaded: true,
+});
+
+models.db
+  //   .sync({ force: true })
+  .sync()
+  .then(() => {
     console.log('db sync successfully');
-})
-.catch(err => console.error(err))
-.finally(()=>{
-    db.close();
-})
+  })
+  .catch(err => console.error(err))
+  .finally(() => {
+    models.db.close();
+  });
