2915ebf9d531fb10346951e5d91902454387a142
Wenyi Zheng <hilarywenyi@gmail.com>
2018-11-12 17:46:56 -0500

    added chaining in db

--- seed.js
+++ seed.js
@@ -5,37 +5,48 @@ const corn = models.Vegetable.create({
   name: 'corn',
   color: 'yellow',
   planted_on: Date.now(),
+}).then((corn)=> {
+    return models.Gardener.create({
+        favoriteVegetableId: corn.id,
+        name: 'Brynn',
+        age: 30
+    });
 });
 
 const kale = models.Vegetable.create({
   name: 'kale',
   color: 'green',
   planted_on: Date.now(),
+}).then((kale)=> {
+    return models.Gardener.create({
+        favoriteVegetableId: kale.id,
+        name: 'Wenyi',
+        age: 26
+    })
+    .then((wenyi)=>{
+      return models.Plot.create({
+          gardenerId: wenyi.id,
+          size: 100,
+          shaded: true,
+      })
+    });
 });
 
 const mushroom = models.Vegetable.create({
   name: 'mushroom',
   color: 'brown',
   planted_on: Date.now(),
+}).then((mushroom)=> {
+    return models.Gardener.create({
+        favoriteVegetableId: mushroom.id,
+        name: 'Corey',
+        age: 30
+    });
 });
 
-const wenyi = models.Gardener.create({
-  name: 'wenyi',
-  age: 26,
-});
-
-const brynn = models.Gardener.create({
-  name: 'brynn',
-  age: 30,
-});
-
-const plot = models.Plot.create({
-  size: 100,
-  shaded: true,
-});
 
 models.db
-  //   .sync({ force: true })
+//    .sync({ force: true })
   .sync()
   .then(() => {
     console.log('db sync successfully');
