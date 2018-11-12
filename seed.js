b5a2fa8ace17b2399fa27167d11639c5fb643073
Brynn Shepherd <brynn.shepherd@gmail.com>
2018-11-12 18:02:45 -0500

    veggie plot association

--- seed.js
+++ seed.js
@@ -5,11 +5,21 @@ const corn = models.Vegetable.create({
   name: 'corn',
   color: 'yellow',
   planted_on: Date.now(),
-}).then((corn)=> {
-    return models.Gardener.create({
-        favoriteVegetableId: corn.id,
-        name: 'Brynn',
-        age: 30
+}).then(corn => {
+  return models.Gardener.create({
+    favoriteVegetableId: corn.id,
+    name: 'Brynn',
+    age: 30,
+  })
+    .then(brynn => {
+      return models.Plot.create({
+        gardenerId: brynn.id,
+        size: 50,
+        shaded: true,
+      });
+    })
+    .then(plot => {
+      return corn.addPlot(plot);
     });
 });
 
@@ -17,18 +27,21 @@ const kale = models.Vegetable.create({
   name: 'kale',
   color: 'green',
   planted_on: Date.now(),
-}).then((kale)=> {
-    return models.Gardener.create({
-        favoriteVegetableId: kale.id,
-        name: 'Wenyi',
-        age: 26
-    })
-    .then((wenyi)=>{
+}).then(kale => {
+  return models.Gardener.create({
+    favoriteVegetableId: kale.id,
+    name: 'Wenyi',
+    age: 26,
+  })
+    .then(wenyi => {
       return models.Plot.create({
-          gardenerId: wenyi.id,
-          size: 100,
-          shaded: true,
-      })
+        gardenerId: wenyi.id,
+        size: 100,
+        shaded: true,
+      });
+    })
+    .then(plot => {
+      return kale.addPlot(plot);
     });
 });
 
@@ -36,17 +49,26 @@ const mushroom = models.Vegetable.create({
   name: 'mushroom',
   color: 'brown',
   planted_on: Date.now(),
-}).then((mushroom)=> {
-    return models.Gardener.create({
-        favoriteVegetableId: mushroom.id,
-        name: 'Corey',
-        age: 30
+}).then(mushroom => {
+  return models.Gardener.create({
+    favoriteVegetableId: mushroom.id,
+    name: 'Corey',
+    age: 30,
+  })
+    .then(corey => {
+      return models.Plot.create({
+        gardenerId: corey.id,
+        size: 200,
+        shaded: false,
+      });
+    })
+    .then(plot => {
+      return mushroom.addPlot(plot);
     });
 });
 
-
 models.db
-//    .sync({ force: true })
+  //   .sync({ force: true })
   .sync()
   .then(() => {
     console.log('db sync successfully');
